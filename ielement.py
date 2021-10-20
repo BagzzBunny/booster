bfefc9cc582c261da38043b0028824ba10bb3e84
Boris <bagzzbanny.bl@gmail.com>
2021-10-20 22:58:24 +0300

    базовый класс

---
+++ ielement.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class IElement(ABC):
+    def calc_M(self): #должен вернуть матрицу
+        pass
+
+    def calc_b(self): #должен вернуть вектор
+        pass
