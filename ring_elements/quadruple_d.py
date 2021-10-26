694a69410d82da09c23325554c0e7524722103ed
Boris <bagzzbanny.bl@gmail.com>
2021-10-26 16:33:41 +0300

    добавил квадруполь дефокусирующей линзы

---
+++ ring_elements/quadruple_d.py
@@ -0,0 +1,20 @@
+from ielement import IElement
+import numpy as np
+
+
+class QuadrupleD(IElement):
+    def calc_M(self, L, K):
+        l_root_k = L*np.sqrt(K)
+        M = np.matrix(
+            [
+                [np.cosh(l_root_k), np.sinh(l_root_k)/np.sqrt(K), 0, 0, 0],
+                [np.sqrt(K)*np.sinh(l_root_k), np.cosh(l_root_k), 0, 0, 0],
+                [0, 0, np.cos(l_root_k), np.sin(l_root_k)/np.sqrt(K), 0],
+                [0, 0, -np.sqrt(K)*np.sin(l_root_k), np.cos(l_root_k), 0],                
+                [0, 0, 0, 0, 1]
+            ]
+        )
+        return M
+
+    def calc_b(self):
+        pass
