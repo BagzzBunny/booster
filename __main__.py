5cb086525ceb42be4f2f9ad6594b0103a696ea0d
Boris <bagzzbanny.bl@gmail.com>
2021-10-27 19:36:45 +0300

    добавил словарь для методов

--- __main__.py
+++ __main__.py
@@ -1,4 +1,8 @@
 from ring_elements.drift import Drift
+from ring_elements.quadrupole_d import QuadrupoleD as qd
+from ring_elements.quadrupole_f import QuadrupoleF as qf
+from ring_elements.corrector import Corrector as corr
+from ring_elements.sbend import Sbend
 from madx_parser import Parser
 import sys
 import csv
@@ -7,10 +11,19 @@ import csv
 #Parser.generate_ring_data('booster.madx')
 #Parser.create_teable()
 ring_elements = {
-    'DRIFT': Drift.calc_M
-    
+    'DRIFT': Drift.calc_dynamics,
+    'QD': qd.calc_dynamics,
+    'QF': qf.calc_dynamics,
+    'KICKER': corr.calc_dynamics,
+    'SBEND': Sbend.calc_dynamics,
+    'SEXTUPOLE': Drift.calc_dynamics
 }
 with open ('Ring-data.csv', newline = '') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        row['KEYWORD']
+        kwargs = {
+            'L': row['L'],
+            
+        }
+        if row['KEYWORD'] == 'QUADRUPOLE':
+            ring_elements(L)
