1f50e92979f0a6bf0ae5aa2a6fc9c37935d1c57d
Boris <bagzzbanny.bl@gmail.com>
2021-11-15 21:25:46 +0300

    переделан решатель

--- utils/solver.py
+++ utils/solver.py
@@ -1,6 +1,5 @@
 from ring_elements.drift import Drift
-from ring_elements.quadrupole_d import QuadrupoleD as qd
-from ring_elements.quadrupole_f import QuadrupoleF as qf
+from ring_elements.quadrupole import Quadrupole
 from ring_elements.corrector import Corrector as corr
 from ring_elements.sbend import Sbend
 import numpy as np
@@ -10,29 +9,32 @@ import csv
 class Solver():
     def __init__(self):
         self.ring_elements = {
-            'DRIFT': Drift.calc_dynamics,
-            'QD': qd.calc_dynamics,
-            'QF': qf.calc_dynamics,
-            'KICKER': corr.calc_dynamics,
-            'SBEND': Sbend.calc_dynamics,
-            'SEXTUPOLE': Drift.calc_dynamics
+            'DRIFT': Drift,
+            'QUADRUPOLE': Quadrupole,
+            'KICKER': corr,
+            'SBEND': Sbend,
+            'SEXTUPOLE': Drift
             }
+
+    def make_element(self, row):
+        if row['KEYWORD'] in self.ring_elements:
+            element = self.ring_elements[row['KEYWORD']](row)
+            return element
+        return None
     
     def solve(self, x):
         trajectory = [x]
         i = 0
-        x = np.matrix([0, 0, 0, 0, 0])
         with open ('Ring-data.csv', newline = '') as csvfile:
             rows = csv.DictReader(csvfile)
             for row in rows:
-                if row['KEYWORD'] == 'QUADRUPOLE':
-                    m, b = self.ring_elements[row['NAME']](L = float(row['L']), K = float(row['K1L']) / float(row['L']))
-                    x = m * trajectory[i] + b
-                elif row['KEYWORD'] not in self.ring_elements:
+                element = self.make_element(row)
+                if element is None:
                     continue
                 else:
-                    m, b = self.ring_elements[row['KEYWORD']](L = float(row['L']), ANGLE = float(row['ANGLE']))
+                    m = element.calc_M()
+                    b = element.calc_b()
                     x = m * trajectory[i] + b
-                trajectory.append(x)
+                    trajectory.append(x)
                 i += 1
         return trajectory
